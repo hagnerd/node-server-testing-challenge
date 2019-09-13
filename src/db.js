50d547cd1cb2330440307a99843c062fb6a9c091
Matt Hagner <web@matthagner.com>
2019-09-12 19:44:19 -0500

    Configure databasec

--- src/db.js
+++ src/db.js
@@ -1,2 +1,12 @@
-const knex = require('knex');
-const config = require('../knexfile');
+const knex = require("knex");
+const config = require("../knexfile");
+
+const environment = process.env.NODE_ENV
+  ? process.env.NODE_ENV === "testing"
+    ? "testing"
+    : "development"
+  : "development";
+
+const db = knex(config[environment]);
+
+module.exports = db;
