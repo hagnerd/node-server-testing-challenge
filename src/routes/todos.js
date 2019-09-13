3e9422dac484c7e92ccd6b41796fd3754837db51
Matt Hagner <web@matthagner.com>
2019-09-12 20:13:49 -0500

    Implement validateTodoInput middleware

---
+++ src/routes/todos.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+
+function validateTodoInput(req, res, next) {
+  const { description } = req.body;
+
+  if (!description) {
+    res.status(400).json({
+      message: "A valid todo must have a description"
+    });
+  } else {
+    next();
+  }
+}
+
+router.post("/", validateTodoInput, async (req, res) => {});
+
+module.exports = router;
